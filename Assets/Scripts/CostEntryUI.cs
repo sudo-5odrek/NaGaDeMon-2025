d83a65ea06b44937761a372b2eaf6d2316a23539
sudo-5odrek <tabaram0@gmail.com>
2025-11-25 19:57:04 +0000

    Buildings now show cost on build button
    title

---
+++ Assets/Scripts/CostEntryUI.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CostEntryUI : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text amountText;
+
+    public void Set(Sprite iconSprite, int amount)
+    {
+        icon.sprite = iconSprite;
+        amountText.text = amount.ToString();
+    }
+}
